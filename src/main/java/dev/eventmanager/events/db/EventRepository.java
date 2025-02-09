30bb8b3972e907d65ac66d78c9bb96df862577dc
ultimathule1 <ranel28021998@gmail.com>
2025-02-09 19:05:26 +0300

    a little refactoring

--- src/main/java/dev/eventmanager/events/db/EventRepository.java
+++ src/main/java/dev/eventmanager/events/db/EventRepository.java
@@ -1,6 +1,5 @@
 package dev.eventmanager.events.db;
 
-import dev.eventmanager.events.domain.EventStatus;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -8,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.List;
 
@@ -19,10 +17,14 @@ public interface EventRepository extends JpaRepository<EventEntity, Long> {
     @Query("""
             SELECT e FROM EventEntity e
             WHERE (:name IS NULL OR e.name = :name)
-            AND (:placesMin <=  e.maxPlaces AND :placesMax > e.maxPlaces)
-            AND (:dateStartBefore <= e.date AND :dateStartAfter > e.date)
-            AND (:costMin <= e.cost AND :costMax > e.cost)
-            AND (:durationMin <= e.duration AND :durationMax > e.duration)
+            AND (:placesMin IS NULL OR :placesMin <= e.maxPlaces)
+            AND (:placesMax IS NULL OR :placesMax > e.maxPlaces)
+            AND (CAST(:dateStartBefore as date) IS NULL OR :dateStartBefore <= e.date)
+            AND (CAST(:dateStartAfter as date) IS NULL OR :dateStartAfter > e.date)
+            AND (:costMin IS NULL OR :costMin <= e.cost)
+            AND (:costMax IS NULL OR :costMax > e.cost)
+            AND (:durationMin IS NULL OR :durationMin <= e.duration)
+            AND (:durationMax IS NULL OR :durationMax > e.duration)
             AND (:locationId IS NULL OR e.locationId = :locationId)
             AND (:eventStatus IS NULL OR e.status = :eventStatus)
             """)
@@ -67,7 +69,7 @@ public interface EventRepository extends JpaRepository<EventEntity, Long> {
             WHERE (e.date + INTERVAL '1 MINUTE' * e.duration) < CURRENT_TIMESTAMP
             AND e.status = :status
             """
-    , nativeQuery = true)
+            , nativeQuery = true)
     List<Long> findEndedEventWithStatus(
             @Param("status") String status
     );
